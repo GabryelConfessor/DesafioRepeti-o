96d5d11481c289290d703bf7f7e875798dc3d7ed
Gabryel Confessor Ferreira <153391024+GabryelConfessor@users.noreply.github.com>
2024-07-01 16:37:27 -0300

    Add files via upload

---
+++ LacosdeRepeticao/src/main/java/com/mycompany/lacosderepeticao/LacosdeRepeticao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lacosderepeticao;
+
+/**
+ *
+ * @author guilherme
+ */
+public class LacosdeRepeticao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
